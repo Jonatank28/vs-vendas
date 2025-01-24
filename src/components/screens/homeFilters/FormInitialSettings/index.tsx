2d6b8c7a6fe3ad3851b259c97ef9a462a2668e8f
Jonatank28 <jonatankalmeidakk28@gmail.com>
2025-01-24 17:50:24 -0300

    yttytyty

--- src/components/screens/homeFilters/FormInitialSettings/index.tsx
+++ src/components/screens/homeFilters/FormInitialSettings/index.tsx
@@ -1,99 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DefaultButtonAction from '@/src/components/DefaultButtonAction';
-import Controller from './Controller';
 import { Pressable, Text, TouchableOpacity, View } from 'react-native';
 import CustomSelect from '@/src/components/forms/CustomSelect';
-import { Ionicons } from "@expo/vector-icons";
-import RNPickerSelect from 'react-native-picker-select';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { StyleSheet } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import Controller from './Controller';
 
-const dataEnterprise = [
-  { label: '1 - Homologado Hidrelec', value: '1' },
-  { label: 'Transforma 2', value: '2' },
-  { label: 'SEIN ÇAAA', value: '3' },
-]
-
-const dataRegion = [
-  { label: 'Centro', value: '1' },
-  { label: 'Norte', value: '2' },
-]
-
-const dataState = [
-  { label: 'AC', value: '1' }, // Acre
-  { label: 'AL', value: '2' }, // Alagoas
-  { label: 'AM', value: '3' }, // Amazonas
-  { label: 'AP', value: '4' }, // Amapá
-  { label: 'BA', value: '5' }, // Bahia
-  { label: 'CE', value: '6' }, // Ceará
-  { label: 'DF', value: '7' }, // Distrito Federal
-  { label: 'ES', value: '8' }, // Espírito Santo
-  { label: 'GO', value: '9' }, // Goiás
-  { label: 'MA', value: '10' }, // Maranhão
-  { label: 'MG', value: '11' }, // Minas Gerais
-  { label: 'MS', value: '12' }, // Mato Grosso do Sul
-  { label: 'MT', value: '13' }, // Mato Grosso
-  { label: 'PA', value: '14' }, // Pará
-  { label: 'PB', value: '15' }, // Paraíba
-  { label: 'PE', value: '16' }, // Pernambuco
-  { label: 'PI', value: '17' }, // Piauí
-  { label: 'PR', value: '18' }, // Paraná
-  { label: 'RJ', value: '19' }, // Rio de Janeiro
-  { label: 'RN', value: '20' }, // Rio Grande do Norte
-  { label: 'RO', value: '21' }, // Rondônia
-  { label: 'RR', value: '22' }, // Roraima
-  { label: 'RS', value: '23' }, // Rio Grande do Sul
-  { label: 'SC', value: '24' }, // Santa Catarina
-  { label: 'SE', value: '25' }, // Sergipe
-  { label: 'SP', value: '26' }, // São Paulo
-  { label: 'TO', value: '27' }  // Tocantins
+const data = [
+  { label: 'Item 1', value: '1' },
+  { label: 'Item 2', value: '2' },
+  { label: 'Item 3', value: '3' },
+  { label: 'Item 4', value: '4' },
+  { label: 'Item 5', value: '5' },
+  { label: 'Item 6', value: '6' },
+  { label: 'Item 7', value: '7' },
+  { label: 'Item 8', value: '8' },
 ];
 
-const FormHomeFilters = () => {
-  const c = Controller()
+const DropdownComponent = () => {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (
+        <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+          Dropdown label
+        </Text>
+      );
+    }
+    return null;
+  };
 
   return (
-    <View className='flex flex-col gap-2'>
-      {/* <CustomSelect data={dataEnterprise} label='Empresa' placeholder='Selecione a empresa' form={c.form} name="enterprise" />
-      <CustomSelect data={dataRegion} label='Região' placeholder='Selecione a região' form={c.form} name="region" />
-      <CustomSelect data={dataState} label='Estado' placeholder='Selecione o estado' form={c.form} name="state" />
-      <View>
-        <Text className='pb-1'>Cidade</Text>
-        <Pressable className='bg-input-bg rounded-md relative flex py-5 px-4' onPress={c.handleOpenModalSearchCity}>
-          <Text>{c.form.getValues('city') || 'Selecione a cidade'}</Text>
-          <TouchableOpacity
-            className="absolute right-4 top-4 "
-          >
-            <Ionicons
-              name='search-outline'
-              size={23}
-              color="#212121"
-            />
-          </TouchableOpacity>
-        </Pressable>
-        {c.form.formState.errors['city']?.message && (
-          <Text className="text-danger-light">
-            {c.form.formState.errors['city'].message as string}
-          </Text>
+    <View style={styles.container}>
+      {renderLabel()}
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? 'Select item' : '...'}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+        renderLeftIcon={() => (
+          <AntDesign
+            style={styles.icon}
+            color={isFocus ? 'blue' : 'black'}
+            name="Safety"
+            size={20}
+          />
         )}
-      </View> */}
-      <RNPickerSelect
-        onValueChange={(value) => console.log(value)}
-        items={[
-          { label: 'Football', value: 'football' },
-          { label: 'Baseball', value: 'baseball' },
-          { label: 'Hockey', value: 'hockey' },
-        ]}
       />
-      <RNPickerSelect
-        onValueChange={(value) => console.log(value)}
-        items={[
-          { label: 'Football', value: 'football' },
-          { label: 'Baseball', value: 'baseball' },
-          { label: 'Hockey', value: 'hockey' },
-        ]}
-      />
-      <DefaultButtonAction title="Carregar dados" onPress={c.form.handleSubmit(c.onSubmit)} />
     </View>
   );
 };
+
+const FormHomeFilters = () => {
+  const c = Controller(); // Ajuste o Controller conforme necessário.
+
+  return (
+    <View style={{ padding: 16 }}>
+      <DropdownComponent />
+      <DefaultButtonAction
+        title="Carregar dados"
+        onPress={c.form.handleSubmit(c.onSubmit)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
 
 export default FormHomeFilters;
